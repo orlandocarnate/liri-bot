9f776a08a7ec6d1d6d13cb53f18798a98d2c62ca
Orlando Carnate <orlando.carnate@gmail.com>
2019-02-11 18:33:55 -0600

    Start with OMDB api and axios

--- liri.js
+++ liri.js
@@ -1,8 +1,14 @@
 require("dotenv").config(); // imports dotenv
-
+var fs = require("fs"); // imports filesystem
+var axios = require("axios"); // Make XMLHttpRequests from the browser
 var keys = require("./keys.js"); // imports keys.js files
 
 var spotify = new spotify(keys.spotify);
+
+var nodeArg = process.argv; // get arguments from command line node liri.js <arguments>
+
+var nodeCommand = nodeArg[2];
+
 
 // concert-this uses the Bands In Town Artist Events API
 // format: node liri.js concert-this <artist/band name here>
@@ -12,6 +18,34 @@ var spotify = new spotify(keys.spotify);
 
 // movie-this - use axios to retrieve data from OMDB API
 // format: node liri.js movie-this '<movie name here>'
+axios.get("http://www.omdbapi.com/?t=" + movName + "&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    // JSON response
+    var movieInfo = "Title: " + response.data.Title + "\n";
+    movieInfo += "Year: " + response.data.Year + "\n";
+    movieInfo = "IMDB Rating: " + response.data.Ratings[0].value + "\n";
+    movieInfo = "Rotten Tomatoes Rating: " + response.data.Ratings[1].value + "\n";
+    movieInfo = "Country: " + response.data.Country + "\n";
+    movieInfo = "Language: " + response.data.Language + "\n";
+    movieInfo = "Plot: " + response.data.Plot + "\n";
+    movieInfo = "Actors: " + response.data.Actors + "\n";
+
+    console.log(movieInfo);
+
+    // PUSH TO FILE
+    addToFile(nodeComand + ":\n" + movieInfo);
+  }
+);
+/*
+   * Title of the movie.
+   * Year the movie came out.
+   * IMDB Rating of the movie.
+   * Rotten Tomatoes Rating of the movie.
+   * Country where the movie was produced.
+   * Language of the movie.
+   * Plot of the movie.
+   * Actors in the movie.
+*/
 
 // do-what-it-says
 // format: node liri.js do-what-it-says
@@ -19,3 +53,20 @@ var spotify = new spotify(keys.spotify);
 // BONUS In addition to logging the data to your terminal/bash 
 // window, output the data to a .txt file called log.txt.
 // Make sure you append each command you run to the log.txt file.
+
+function addToFile(arg) {
+    fs.appendFile("log.txt", arg);
+}
+/* 
+fs.writeFile("mymovies.txt", "Interstellar, Tron", function(err) {
+    // If the code experiences any errors it will log the error to the console.
+    if (err) {
+      return console.log(err);
+    }
+    // Otherwise, it will print: "movies.txt was updated!"
+    console.log("movies.txt was updated!");
+  });
+  */
+
+
+  
